b9d746b54fe6bcccfbd0cdfb3c0bbe271104c4de
Victory Amunga <83257399+Omuk@users.noreply.github.com>
2024-02-15 18:46:00 -0800

    data

--- SQL/sqlstep.py
+++ SQL/sqlstep.py
@@ -3,22 +3,3 @@ import numpy as np
 import mysql.connector 
 from mysql.connector import Error
 
-
-def create_server_connection(host_name, user_name,user_password):
-
-    connection = None 
-
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name, 
-            paswd = user_password
-        )
-    
-        print('MySQL Database Connection Successful')
-
-    except Error as err:
-        print(f"Error: '{err}'")
-    
-    return connection
-
